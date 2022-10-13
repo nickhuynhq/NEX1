e5a1a89884e531bff6a6a344e46fbb4dfacba5a8
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-13 14:22:14 -0400

    Complete styling and formatting for Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+// List component to be used in Footer component
+const List = ({ items, mt }: { items: string[]; mt: boolean }) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {items.map((item) => (
+      <p
+        key={item}
+        className="text-grey-400 text-sm hover:underline cursor-pointer"
+      >
+        {item}
+      </p>
+    ))}
+  </div>
+);
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt />
+      <List items={footerList3} mt />
+      <p className="text-grey-400 text-sm mt-5">2022 NextReel</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
