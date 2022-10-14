9bcc7dce14c660d7ef343c100d82c8892ae2b749
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-14 11:34:36 -0400

    Set up Video Details page and set Link route on VideoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -72,7 +72,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           onMouseLeave={() => setIsHover(false)}
           className="rounded-3xl"
         >
-          <Link href="/">
+          <Link href={`/detail/${post._id}`}>
             <video
               loop
               ref={videoRef}
