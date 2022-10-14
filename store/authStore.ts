9608366891c93934ec725598bac426b3bcefb2ab
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-14 00:34:01 -0400

    Add user upload button, and implement logout functionality

--- store/authStore.ts
+++ store/authStore.ts
@@ -7,7 +7,8 @@ import axios from 'axios';
 const authStore = (set: any) => ({
     userProfile: null,
 
-    addUser: (user: any) => set({ userProfile: user })
+    addUser: (user: any) => set({ userProfile: user }),
+    removeUser: () => set({userProfile : null})
 });
 
 const useAuthStore = create(
