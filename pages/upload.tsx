77170c8f6c750b7bce6b1d1a11b67d492f567ce5
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-14 11:11:42 -0400

    Finish styling and formatting Video Upload page

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,6 +7,7 @@ import { SanityAssetDocument } from "@sanity/client";
 
 import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
+import { topics } from "../utils/constants";
 
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -36,8 +37,8 @@ const Upload = () => {
   };
 
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-white rounded-lg">
+    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className="bg-white rounded-lg xl:h=[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -53,12 +54,11 @@ const Upload = () => {
                 {videoAsset ? (
                   <div>
                     <video
-                        src={videoAsset.url}
-                        loop
-                        controls
-                        className="rounded-xl h-[450px] mt-16 bg-black"
-                    >
-                    </video>
+                      src={videoAsset.url}
+                      loop
+                      controls
+                      className="rounded-xl h-[450px] mt-16 bg-black"
+                    ></video>
                   </div>
                 ) : (
                   <label className="cursor-pointer">
@@ -89,8 +89,50 @@ const Upload = () => {
                 )}
               </div>
             )}
+            {wrongFileType && (
+              <p className="text-center text-xl text-red-400 text-semibold mt-4 w-[250px]">
+                Please select a video file
+              </p>
+            )}
           </div>
         </div>
+        <form className="flex flex-col gap-3 pb-10">
+          <label className="text-md font-medium">Caption</label>
+          <input
+            type="text"
+            value=""
+            onChange={() => {}}
+            className="rounded outline-none text-md border-2 border-gray-200 p-2"
+          />
+          <label className="text-md font-medium">Choose a Category</label>
+          <select className="outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer" onChange={() => {}}>
+            {topics.map((topic) => (
+                <option
+                    key={topic.name}
+                    className="outline-none capitalize bg-white text-gray-700 text-md p-2 hover:bg-slate-300"
+                    value={topic.name}
+                >
+                    {topic.name}
+                </option>
+            ))}
+          </select>
+          <div className="flex gap-6 mt-10">
+            <button 
+                onClick={()=>{}}
+                type="button"
+                className="border-gray-300 border-2 text-md font-mediunm p-2 rounded w-28 lg:w-44 outline-none"
+            >
+                Discard
+            </button>
+            <button 
+                onClick={()=>{}}
+                type="button"
+                className="bg-[#F51997] text-white border-2 text-md font-mediunm p-2 rounded w-28 lg:w-44 outline-none"
+            >
+                Post
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
