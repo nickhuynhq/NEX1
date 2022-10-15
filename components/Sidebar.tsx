dfbde1a2a8c617e7854fcc6a1651a2f16ca46c88
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-15 15:02:09 -0400

    Remove more unnessary imports

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import Link from "next/link";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { ImCancelCircle } from "react-icons/im";
