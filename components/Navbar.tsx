895e58127da7ae9c51ea047461271c14100139f9
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-15 11:39:08 -0400

    Create search bar component and handle search function

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
@@ -11,10 +11,19 @@ import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
 
 const Navbar = () => {
-
   // Hook to store user information persistently
-  const {userProfile, addUser, removeUser} = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState("");
+  const router = useRouter();
 
+  const handleSearch = (e: {preventDefault: () => void}) => {
+    e.preventDefault();
+
+    if(searchValue){
+      router.push(`/search/${searchValue}`)
+    }
+  }
+  
   return (
     <nav className="w-full flex justify-between items-center border-b-2 border-gray-200 py-4 px-4">
       <Link href="/">
@@ -27,15 +36,29 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>
-        Search
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[260px] md:w-[350px] rounded-full md:top-0"
+          />
+          <button onClick={handleSearch} className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400">
+            <BiSearch />
+          </button>
+        </form>
       </div>
       <div>
         {userProfile ? (
           <div className="flex gap-5 md:gap-10">
             <Link href="/upload">
-              <button className="border-2 px-2 md: px-4 text-md font-semibold flex items-center gap-2">
-                <IoMdAdd className="text-xl"/> {` `}
+              <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
                 <span className="hidden md:block">Upload</span>
               </button>
             </Link>
@@ -50,19 +73,23 @@ const Navbar = () => {
                     alt="profile photo"
                   />
                 </>
-            </Link>
+              </Link>
             )}
-            <button type="button" className="px-2" onClick={() => {
-              googleLogout();
-              removeUser();
-            }}>
-              <AiOutlineLogout color="red" fontSize={21}/>
+            <button
+              type="button"
+              className="px-2"
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}
+            >
+              <AiOutlineLogout color="red" fontSize={21} />
             </button>
           </div>
         ) : (
-          <GoogleLogin 
+          <GoogleLogin
             onSuccess={(response) => createOrGetUser(response, addUser)}
-            onError={()=> console.log("Error")}
+            onError={() => console.log("Error")}
           />
         )}
       </div>
