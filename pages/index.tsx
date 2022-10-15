11c36d316eed758c84177e55e6d406fb2c8e8250
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-15 12:23:05 -0400

    Add filter by topic functionality on discover component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,46 @@
-import axios from 'axios';
-import NoResults from '../components/NoResults';
-import VideoCard from '../components/VideoCard';
-import {Video} from '../types';
-import { BASE_URL } from '../utils';
+import axios from "axios";
+import NoResults from "../components/NoResults";
+import VideoCard from "../components/VideoCard";
+import { Video } from "../types";
+import { BASE_URL } from "../utils";
 
 interface IProps {
-  videos: Video[]
+  videos: Video[];
 }
 
-const Home  = ({ videos }: IProps) => {
+const Home = ({ videos }: IProps) => {
   return (
-    <div className='flex flex-col gap-10 videos h-full'>
+    <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (
-        videos.map((video: Video) => (
-          <VideoCard post={video} key={video._id}/>
-        ))
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
       ) : (
-        <NoResults text={'No videos'}/>
+        <NoResults text={`No Videos`} />
       )}
     </div>
-  )
+  );
 };
 
 // Prerender page on each request using the data returned
 // Only use when you want to render a page whose data must be fetched at request time
-export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`);
+export const getServerSideProps = async ({
+  query: { topic },
+}: {
+  query: { topic: string };
+}) => {
+  let response = null;
+
+  // If topic is selected, show only videos related to that topic
+  if (topic) {
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api/post`);
+  }
 
   return {
     props: {
-      videos: data
-    }
-  }
-}
+      videos: response.data,
+    },
+  };
+};
 
 export default Home;
